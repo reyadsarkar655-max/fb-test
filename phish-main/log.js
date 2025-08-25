0467c694acea5cd62b5b112f2300d58aaa961a86
reyadsarkar655-max <reyadsarkar655@gmail.com>
2025-08-25 17:38:54 +0600

    Delete phish-main directory

--- phish-main/log.js
+++
@@ -1,9 +0,0 @@
-const logSuccess = (email, password) => {
-    console.log(`Success | Time: ${new Date().toLocaleString()} | Email: ${email} | Password: ${password}`);
-};
-
-const logError = (errorMessage) => {
-    console.error(`Error | Time: ${new Date().toLocaleString()} | Message: ${errorMessage}`);
-};
-
-module.exports = { logSuccess, logError };
